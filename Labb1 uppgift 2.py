65e38ca5e1b96a352468840bc8f580bc6ecab41a
matteusskagius <78982036+matteusskagius@users.noreply.github.com>
2021-04-06 17:41:37 +0200

    Update Labb1 uppgift 2.py

--- Labb1 uppgift 2.py
+++ Labb1 uppgift 2.py
@@ -58,5 +58,35 @@ print()
 print()
 
 print("4)")                 #4
+print()
+def sockerkaka(antal, Antal):
+        print("Ingredienser")
+        print("Ägg", int((3/4)*antal),"st")         
+        print("Strösocker",(3/4)*antal,"dl")              
+        print("Vaniljsocker",(2/4)*antal,"tsk")              
+        print("Bakpulver",(2/4)*antal,"tsk")              
+        print("Vetemjöl",(3/4)*antal,"dl")              
+        print("Smör",(75/4)*antal,"g")             
+        print("Vatten",(1/4)*antal,"dl")
+        print()
+        print("Tid")
+        print("Omrörningstid",(10+1*antal))  
+        print("tid grädda",(30+3*antal))
+
+        print()
+        print("Ingredienser")
+        print("Ägg", int((3/4)*Antal),"st")         
+        print("Strösocker",(3/4)*Antal,"dl")              
+        print("Vaniljsocker",(2/4)*Antal,"tsk")              
+        print("Bakpulver",(2/4)*Antal,"tsk")              
+        print("Vetemjöl",(3/4)*Antal,"dl")              
+        print("Smör",(75/4)*Antal,"g")             
+        print("Vatten",(1/4)*Antal,"dl")
+        print()
+        print("Tid")
+        print("Omrörningstid",(10+1*Antal))  
+        print("tid grädda",(30+3*Antal))
+
+sockerkaka(p, q)
 
 
