82f272545b3bf894c8b4b8cf51dfb2d3c45af0db
matteusskagius <78982036+matteusskagius@users.noreply.github.com>
2021-04-07 09:07:11 +0200

    Update Labb1 uppgift 2.py

--- Labb1 uppgift 2.py
+++ Labb1 uppgift 2.py
@@ -1,9 +1,8 @@
-p=6
-
-def recept(antal):
+def recept(antal):  #definierar recept som funktion
     print("Personer",antal)
-    print()
-    print("1)")             #1
+    #print()
+    #print("1)")             #1
+    #print()
     print("Ägg", int((3/4)*antal),"st")         
     print("Strösocker",(3/4)*antal,"dl")              
     print("Vaniljsocker",(2/4)*antal,"tsk")              
@@ -12,81 +11,40 @@ def recept(antal):
     print("Smör",(75/4)*antal,"g")             
     print("Vatten",(1/4)*antal,"dl")
 
-
-
-recept(p)
-
-
-print()
-print()
-print()
-print("2)")                 #2
+#print("2)")                 #2
 def tidBlanda(antal):
     print("Omrörningstid",(10+1*antal))
-
-tidBlanda(p)
-
 
 def tidGradda(antal):
     print("tid grädda",(30+3*antal))
 
-tidGradda(p)
 
 
-print()
-print()
-print()
-print("3)")                 #3
+
+#print("3)")                 #3
+
 def sockerkaka(antal):
-        print("Ingredienser")
-        print("Ägg", int((3/4)*antal),"st")         
-        print("Strösocker",(3/4)*antal,"dl")              
-        print("Vaniljsocker",(2/4)*antal,"tsk")              
-        print("Bakpulver",(2/4)*antal,"tsk")              
-        print("Vetemjöl",(3/4)*antal,"dl")              
-        print("Smör",(75/4)*antal,"g")             
-        print("Vatten",(1/4)*antal,"dl")
-        print()
-        print("Tid")
-        print("Omrörningstid",(10+1*antal))  
-        print("tid grädda",(30+3*antal))
+        recept(antal)
+        tidBlanda(antal)
+        tidGradda(antal)
+            
+        
+    
+        
 
-sockerkaka(p)
+#sockerkaka(p)
 
-print()
-print()
-print()
+
+
+
 
 print("4)")                 #4
+
+
+
+        
+
+sockerkaka(4)
 print()
-def sockerkaka(antal, Antal):
-        print("Ingredienser")
-        print("Ägg", int((3/4)*antal),"st")         
-        print("Strösocker",(3/4)*antal,"dl")              
-        print("Vaniljsocker",(2/4)*antal,"tsk")              
-        print("Bakpulver",(2/4)*antal,"tsk")              
-        print("Vetemjöl",(3/4)*antal,"dl")              
-        print("Smör",(75/4)*antal,"g")             
-        print("Vatten",(1/4)*antal,"dl")
-        print()
-        print("Tid")
-        print("Omrörningstid",(10+1*antal))  
-        print("tid grädda",(30+3*antal))
-
-        print()
-        print("Ingredienser")
-        print("Ägg", int((3/4)*Antal),"st")         
-        print("Strösocker",(3/4)*Antal,"dl")              
-        print("Vaniljsocker",(2/4)*Antal,"tsk")              
-        print("Bakpulver",(2/4)*Antal,"tsk")              
-        print("Vetemjöl",(3/4)*Antal,"dl")              
-        print("Smör",(75/4)*Antal,"g")             
-        print("Vatten",(1/4)*Antal,"dl")
-        print()
-        print("Tid")
-        print("Omrörningstid",(10+1*Antal))  
-        print("tid grädda",(30+3*Antal))
-
-sockerkaka(p, q)
-
-
+print()
+sockerkaka(7)
